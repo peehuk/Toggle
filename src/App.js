d60acf6b2a63d40da3be2a7be9ca11db4b292a71
Harshita kaushik <113887269+peehuk@users.noreply.github.com>
2022-09-23 16:44:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: true };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleClick() {
+    this.setState((state) => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>WELCOME USER</h1>
+        <h2>Please check your Credentials and Enter</h2>
+        <form id="welcome" action="" method="get" />
+        <label for="firstname">First name: </label>
+        <input type="text" id="firstname" name="firstname" value="HARSHITA" />
+        <br />
+        <br />
+        <label for="lastname">Last name: </label>
+        <input type="text " id="lastname" name="lastname" value="KAUSHIK" />
+        <br />
+        <br />
+        Female:
+        <input type="checkbox" id="box" name="check" value="box" />
+        <br />
+        Male:
+        <input type="checkbox" id="box" name="check" value="box" />
+        <br />
+        Others:
+        <input type="checkbox" id="box" name="check" value="box" />
+        <br /> <br />
+        <button onClick={() => this.handleClick()}>
+          {this.state.isToggleOn ? "SUBMIT" : "UNSUBMIT"}
+        </button>
+        <form />
+      </div>
+    );
+  }
 }
 
 export default App;
